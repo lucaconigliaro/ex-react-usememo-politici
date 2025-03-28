16022a048174985705cbfbc97107fadfb58996be
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-03-28 21:40:36 +0100

    Bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 
-const baseUrl = `https://boolean-spec-frontend.vercel.app/freetestapi/politicians`
+const baseUrl = `https://boolean-spec-frontend.vercel.app/freetestapi/politicians`;
 
 function Card({ name, image, position, biography }) {
   console.log(`Card`);
@@ -19,6 +19,7 @@ const MemorizedCard = React.memo(Card);
 function App() {
   const [politician, setPolitician] = useState([]);
   const [search, setSearch] = useState("");
+  const [selectedPosition, setSelectedPosition] = useState("");
 
   async function getPoliticians() {
     const resp = await fetch(baseUrl);
@@ -30,12 +31,24 @@ function App() {
     getPoliticians();
   }, []);
 
+  const position = useMemo(() => {
+    const uniquePositions = [];
+    politician.forEach(politician => {
+      if (!uniquePositions.includes(politician.position)) {
+        uniquePositions.push(politician.position);
+      }
+    });
+    return uniquePositions;
+  }, [politician]);
+
   const filteredPoliticians = useMemo(() => {
-    return politician.filter(politician =>
-      politician.name.toLowerCase().includes(search.toLowerCase()) ||
-      politician.biography.toLowerCase().includes(search.toLowerCase())
-    );
-  }, [politician, search]);
+    return politician.filter(politician => {
+      const isNameMatch = politician.name.toLowerCase().includes(search.toLowerCase()) ||
+        politician.biography.toLowerCase().includes(search.toLowerCase());
+      const isPositionMatch = selectedPosition ? politician.position === selectedPosition : true;
+      return isNameMatch && isPositionMatch;
+    });
+  }, [politician, search, selectedPosition]);
 
   return (
     <div className="container">
@@ -46,10 +59,21 @@ function App() {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
+      <select
+        value={selectedPosition}
+        onChange={(e) => setSelectedPosition(e.target.value)}
+      >
+        <option value="">Tutte le posizioni</option>
+        {position.map((position, index) => (
+          <option key={index} value={position}>
+            {position}
+          </option>
+        ))}
+      </select>
       {filteredPoliticians.length > 0 ? (
         <div className="card-container">
           {filteredPoliticians.map((pol) => (
-            <MemorizedCard key={pol.id} {...pol}/>
+            <MemorizedCard key={pol.id} {...pol} />
           ))}
         </div>
       ) : (
